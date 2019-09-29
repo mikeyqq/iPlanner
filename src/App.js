802f1d1172cb5fba3e28bd8d8d0250b0a9c0839d
Anchen <acmhuang@gmail.com>
2019-09-29 14:00:37 -0400

    containers/page-added login, register, main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Login from "../src/container/login/Login";
+import Register from "../src/container/register/Register";
+import Home from "../src/container/home/Home";
 
 function App() {
   return (
     <div>
       <h1>hello</h1>
+      <Login />
+      <Register />
+      <Home />
     </div>
   );
 }
